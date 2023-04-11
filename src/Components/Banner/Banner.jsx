574c07e58ee6f653fcd49a0506cac07f7e64d36f
priyab99 <priya.bd.9911@gmail.com>
2023-04-11 22:37:55 +0600

    readme file added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                 <div>
                     <h1 className='font-extrabold text-7xl'>One Step Closure To My</h1>
                     <h3 className='text-7xl font-extrabold text-blue-700 my-5'>Dream Job</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus magnam et quaerat, ipsam quo fugiat repellat? Ab temporibus ipsum facilis blanditiis eaque iste cumque perspiciatis nisi, accusamus neque iusto, aperiam quo ut. Voluptate est, consectetur dignissimos minus quam laudantium officia velit aperiam impedit culpa, repudiandae officiis aliquid veritatis consequuntur deserunt tempore! Nam, ratione obcaecati? Perspiciatis illum enim odit voluptatibus possimus.</p>
+                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                     <button className='button mt-5'>Get Started</button>
 
                 </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className=' mt-12 p-28 flex justify-between bg-blue-50'>
-            <h1 className='text-lg font-extrabold'>My Carrer</h1>
+            <h1 className='text-lg font-extrabold'>Best Career Opportunities</h1>
             <div className='anchor'>
                 <a href="/">Home</a>
                 <a href="/statistics">Statistics</a>
