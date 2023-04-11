9521c64f549d5a2aa7acde762e86579c71bc6679
priyab99 <priya.bd.9911@gmail.com>
2023-04-11 15:27:39 +0600

    job details page done

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import './JobDetails.css'
 
 const JobDetails = () => {
     const {jobId}=useParams();
@@ -13,22 +14,36 @@ const JobDetails = () => {
       }, [jobs, jobId]);
       
 
-    const {jobResponsibility,educationalRequirements,experiences}=jobDetails;
+    const {jobDescription,jobResponsibility,educationalRequirements,experiences,jobTitle,salary,phone,email}=jobDetails;
 
     
       
     return (
         <div>
-            <h2 className=' text-lg text-center '>Job Details</h2>
+            <h2 className=' text-lg text-center font-extrabold'>Job Details</h2>
 
-            <div className='flex'>
-            <div>
-              <h3>{experiences}</h3>
+            <div className='flex w-3/4 h-2/4 mt-10 p-28 text-center mx-52'>
+            <div className='job'>
+                <h1 ><span className='job-style'>Job Description:</span>:{jobDescription}</h1>
+                <p><span className='job-style'>Job Resposibility:</span> {jobResponsibility}</p>
+                <p><span className='job-style'>Educational Requirements:</span>{educationalRequirements}</p>
+                <h3><span className='job-style'>Experiences</span> {experiences}</h3>
 
               </div>
-               <div>
+               <div className='job-details'>
+                <h1 className='text-lg text-center font-extrabold'>Job Details</h1>
+                <p><span className='job-style'>Salary:</span>{salary}</p>
+                <p><span className='job-style'>Job Title:</span> {jobTitle}</p>
+                <h1 className='text-center ml-2 font-bold'>Contact Infromation</h1>
+                <p><span className='job-style'>Phone:</span>{phone}</p>
+                <p><span className='job-style'>email:</span>{email}</p>
+
+
+                <button className='apply-btn'>Apply Now</button>  
+
     
             </div>
+           
             </div>
 
         </div>
