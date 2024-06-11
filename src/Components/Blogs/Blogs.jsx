cf658a7053ad8b04f4552763f0c2b015478c6168
pietaa8 <tasnianafisa509@gmail.com>
2024-06-11 19:45:26 +0600

    first commit

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            <h1 className='text-lg font-semibold  mt-10'>When should I use context API?</h1>
-            <p>Context is primarily used when some data needs to be accessible by many components at different nesting levels. Apply it sparingly because it makes component reuse more difficult. If I only want to avoid passing some props through many levels, component composition is often a simpler solution than context.</p>
-            <h1 className='text-lg font-semibold  mt-10'> What is a custom hook?</h1>
-            <p>Both components and Hooks are functions, so this works for them too! A custom Hook is a JavaScript function whose name starts with ” use ” and that may call other Hooks.</p>
-            <h1 className='text-lg font-semibold  mt-10'>What is useRef?</h1>
-            <p>useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference (aka ref). A reference is an object having a special property current .</p>
-            <h1 className='text-lg font-semibold  mt-10'>What is useMemo?</h1>
-            <p>useMemo() is a function that returns a memoized value of a passed in resource-intensive function. It is very useful in optimizing the performance of a React component by eliminating repeating heavy computations.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-lg font-semibold  mt-10">
+        When should I use context API?
+      </h1>
+      <p>
+        The Context API in React is best used when passing data or state down to
+        multiple components at different levels of nesting in the component
+        tree, particularly for managing global state like user authentication,
+        themes, or language preferences. Context simplifies data sharing and
+        eliminates the need for manual prop passing.
+      </p>
+      <h1 className="text-lg font-semibold  mt-10"> What is a custom hook?</h1>
+      <p>
+        A custom hook in React is a JavaScript function that starts with "use"
+        and allows you to extract component logic into reusable functions. These
+        hooks can encapsulate stateful logic, side effects, or any custom
+        behavior, promoting code reusability and maintainability across multiple
+        components. Custom hooks enable cleaner and more modular code by
+        separating concerns and promoting composability.
+      </p>
+      <h1 className="text-lg font-semibold  mt-10">What is useRef?</h1>
+      <p>
+        useRef is a React hook that returns a mutable ref object whose .current
+        property is initialized to the passed argument. It's commonly used to
+        access and interact with DOM elements directly, store mutable values
+        across renders without causing re-renders, or persist values between
+        renders in functional components without causing side effects.
+      </p>
+      <h1 className="text-lg font-semibold  mt-10">What is useMemo?</h1>
+      <p>
+        useMemo is a React hook that memoizes the result of a function and
+        re-computes it only if the dependencies change. It's useful for
+        optimizing performance by avoiding unnecessary re-calculations of
+        expensive computations or calculations in components that render
+        frequently. useMemo is particularly beneficial when dealing with
+        computationally intensive operations or complex data transformations
+        within functional components.
+      </p>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,64 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './Components/Home/Home';
-import Homepage from './Components/Homepage/Homepage';
-import AppliedJobs from './Components/AppliedJobs/AppliedJobs';
-import Blogs from './Components/Blogs/Blogs';
-import JobDetails from './Components/JobDetails/JobDetails';
-import Statistics from './Components/Statistics/Statistics';
-import Error from './Components/Error/Error';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Homepage from "./Components/Homepage/Homepage";
+import AppliedJobs from "./Components/AppliedJobs/AppliedJobs";
+import Blogs from "./Components/Blogs/Blogs";
+import JobDetails from "./Components/JobDetails/JobDetails";
+import Statistics from "./Components/Statistics/Statistics";
+import Error from "./Components/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Home></Home>,
-    children:[
+    element: <Home></Home>,
+    children: [
       {
-        path:"/",
-        element:<Homepage></Homepage>,
-        loader: ()=>fetch('jobs.json')
+        path: "/",
+        element: <Homepage></Homepage>,
+        loader: () => fetch("jobs.json"),
       },
       {
-        path:"/statistics",
-        element:<Statistics></Statistics>
-
+        path: "/statistics",
+        element: <Statistics></Statistics>,
       },
       {
-        path:"job/:jobId",
-        element:<JobDetails></JobDetails>,
-        loader:()=>fetch('/jobs.json')
-
-
-
+        path: "job/:jobId",
+        element: <JobDetails></JobDetails>,
+        loader: () => fetch("/jobs.json"),
       },
       {
-        path:"/appliedJobs",
-        element:<AppliedJobs></AppliedJobs>
+        path: "/appliedJobs",
+        element: <AppliedJobs></AppliedJobs>,
       },
       {
-        path:"/blogs",
-        element:<Blogs></Blogs>
+        path: "/blogs",
+        element: <Blogs></Blogs>,
       },
       {
-        path:"*",
-        element:<Error/>
-  
-      }
-      
-    ]
-
-   
-  }
+        path: "*",
+        element: <Error />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
